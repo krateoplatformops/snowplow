874e6dda207c0cd526bfcff2d485eb583d20a225
Luca Sepe <luca.sepe@gmail.com>
2025-04-02 08:38:13 +0200

    feat: log parsed gvr, name and namespace from call query params

--- internal/handlers/dispatchers/helpers.go
+++ internal/handlers/dispatchers/helpers.go
@@ -1,25 +1,31 @@
 package dispatchers
 
 import (
+	"log/slog"
 	"net/http"
 
 	"github.com/krateoplatformops/snowplow/internal/handlers/util"
 	"github.com/krateoplatformops/snowplow/internal/objects"
+	xcontext "github.com/krateoplatformops/snowplow/plumbing/context"
 	"github.com/krateoplatformops/snowplow/plumbing/http/response"
 )
 
 func fetchObject(req *http.Request) (got objects.Result) {
+	log := xcontext.Logger(req.Context())
+
 	gvr, err := util.ParseGVR(req)
 	if err != nil {
 		got.Err = response.New(http.StatusBadRequest, err)
 		return
 	}
+	log.Debug("GVR from request query parameters", slog.Any("gvr", gvr))
 
 	nsn, err := util.ParseNamespacedName(req)
 	if err != nil {
 		got.Err = response.New(http.StatusBadRequest, err)
 		return
 	}
+	log.Debug("Name and Namespace from request query parameters", slog.Any("nsn", nsn))
 
 	return objects.Get(req.Context(), objects.Reference{
 		Name: nsn.Name, Namespace: nsn.Namespace,
