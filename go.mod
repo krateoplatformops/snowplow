b69064beb7082e9605fd16787f17c1e8d8bcc773
Luca Sepe <luca.sepe@gmail.com>
2025-08-22 11:44:07 +0200

    feat: align widgetdata crd validation with kube apiserver strict mode (#143)

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/krateoplatformops/snowplow
 go 1.24.2
 
 require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc
 	github.com/google/go-cmp v0.7.0
 	github.com/itchyny/gojq v0.12.17
 	github.com/krateoplatformops/plumbing v0.6.2
@@ -27,7 +28,6 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
 	github.com/evanphx/json-patch/v5 v5.9.11 // indirect
 	github.com/fatih/color v1.18.0 // indirect
--- internal/resolvers/crds/schema/validate.go
+++ internal/resolvers/crds/schema/validate.go
@@ -2,10 +2,69 @@ package schema
 
 import (
 	"errors"
+	"fmt"
 
 	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
+	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apiextensions-apiserver/pkg/apiserver/validation"
+	"sigs.k8s.io/yaml"
 )
+
+func buildValidationFromSchemaData(data map[string]any) (*apiextensions.CustomResourceValidation, error) {
+	// 1. Set additionalProperties=false
+	enforceStrictObjects(data)
+
+	// 2. From map to YAML
+	yml, err := yaml.Marshal(data)
+	if err != nil {
+		return nil, fmt.Errorf("marshal to YAML: %w", err)
+	}
+
+	var schemaV1 apiextv1.JSONSchemaProps
+	if err := yaml.Unmarshal(yml, &schemaV1); err != nil {
+		return nil, fmt.Errorf("unmarshal to v1 JSONSchemaProps: %w", err)
+	}
+
+	// 3. From v1.JSONSchemaProps to internal JSONSchemaProps
+	var schemaInternal apiextensions.JSONSchemaProps
+	if err := apiextv1.Convert_v1_JSONSchemaProps_To_apiextensions_JSONSchemaProps(
+		&schemaV1, &schemaInternal, nil); err != nil {
+		return nil, fmt.Errorf("convert v1→internal: %w", err)
+	}
+
+	return &apiextensions.CustomResourceValidation{
+		OpenAPIV3Schema: &schemaInternal,
+	}, nil
+}
+
+// enforceStrictObjects set additionalProperties=false on all nodes (type: object)
+func enforceStrictObjects(node map[string]any) {
+	if nodeType, ok := node["type"].(string); ok && nodeType == "object" {
+		if _, exists := node["additionalProperties"]; !exists {
+			node["additionalProperties"] = false
+		}
+	}
+
+	// Recursion for properties
+	if props, ok := node["properties"].(map[string]any); ok {
+		for _, v := range props {
+			if child, ok := v.(map[string]any); ok {
+				enforceStrictObjects(child)
+			}
+		}
+	}
+
+	// Recursion for items (array)
+	if items, ok := node["items"].(map[string]any); ok {
+		enforceStrictObjects(items)
+	} else if itemsSlice, ok := node["items"].([]any); ok {
+		for _, it := range itemsSlice {
+			if child, ok := it.(map[string]any); ok {
+				enforceStrictObjects(child)
+			}
+		}
+	}
+}
 
 func validateCustomResource(crv *apiextensions.CustomResourceValidation, doc map[string]any) error {
 	validator, _, err := validation.NewSchemaValidator(crv.OpenAPIV3Schema)
